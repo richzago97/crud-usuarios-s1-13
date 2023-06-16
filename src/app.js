c81d1bdf8b9e6cc6222d81e98f0b1ac9c6f26c55
richzago97 <rich.czago@gmail.com>
2023-06-16 11:01:48 -0300

    feat: adicionado nova rota da documentação

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import userRouter from "./routes/users.routes";
 import loginRoutes from "./routes/session.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 app.use(express.json());
@@ -11,8 +13,10 @@ const PORT = process.env.PORT || 3000;
 app.use("/users", userRouter);
 app.use("/login", loginRoutes);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.listen(PORT, () => {
-  console.log(`server is running at port ${PORT}`);
+   console.log(`server is running at port ${PORT}`);
 });
 
 export default app;
