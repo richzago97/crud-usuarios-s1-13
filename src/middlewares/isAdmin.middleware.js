df75f0d832177f62dcb62bd3ef4a2428f253f2c5
richzago97 <rich.czago@gmail.com>
2023-06-16 11:02:04 -0300

    feat: adicionado mais uma camada de validação

--- src/middlewares/isAdmin.middleware.js
+++ src/middlewares/isAdmin.middleware.js
@@ -1,16 +1,24 @@
 import users from "../database";
 
 const isAdminMiddleware = (req, res, next) => {
-  const email = req.email;
-  const user = users.find((user) => user.email === email);
-  const { uuid } = req.params;
-  if (user.isAdm === false && user.uuid != uuid) {
-    return res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
-  
-  next();
+   const email = req.email;
+   const user = users.find((user) => user.email === email);
+
+   if (!user) {
+      return res.status(401).json({
+         message: "Unauthorized",
+      });
+   }
+
+   const { uuid } = req.params;
+
+   if (user.isAdm === false && user.uuid != uuid) {
+      return res.status(401).json({
+         message: "Unauthorized",
+      });
+   }
+
+   next();
 };
 
 export default isAdminMiddleware;
